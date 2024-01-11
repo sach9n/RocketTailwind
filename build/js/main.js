7d322af385293c1eb19ff591a20a82893b617e7f
sach9n <66356517+sach9n@users.noreply.github.com>
2024-01-11 23:31:21 +0530

    css changes

---
+++ build/js/main.js
@@ -0,0 +1,16 @@
+const initApp = () => {
+    const hamberger = document.getElementById('hamburger-button');
+    const mobileMenu = document.getElementById('mobile-menu');
+
+    const toggleMenu = () => {
+        mobileMenu.classList.toggle('hidden');
+        mobileMenu.classList.toggle('flex');
+        hamberger.classList.toggle('toggel-btn');
+    }
+
+    hamberger.addEventListener('click', toggleMenu);
+    mobileMenu.addEventListener('click', toggleMenu);
+
+}
+
+document.addEventListener('DOMContentLoaded' , initApp)
